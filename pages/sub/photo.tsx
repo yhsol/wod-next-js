77e1b22db71b54d1c00aa75698be5be41107864f
yhsol <loshy244110@gmail.com>
2022-10-25 01:25:32 +0900

    feat: change test for screenshots

--- pages/sub/photo.tsx
+++ pages/sub/photo.tsx
@@ -4,6 +4,7 @@ const Photo = () => {
   return (
     <div>
       <h1>Photo</h1>
+      <h2>some changes</h2>
       <Image
         src={
           "https://media.gettyimages.com/photos/the-natural-arch-of-drangarnir-beach-view-from-a-cliff-around-in-picture-id1413257801"
